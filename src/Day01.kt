b82d7b351dc52a400fcaea1344daa4b9b17e7327
Alexander Lang <Alexander.Lang@cloudflight.io>
2021-12-01 20:19:34 +0100

    Solution Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,21 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }.zipWithNext().fold(0) { acc, (last, current) ->
+            acc + (if (current > last) 1 else 0)
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val sums = input.map { it.toInt() }.windowed(3).map { it.sum() }
+        return sums.zipWithNext().fold(0) { acc, (last, current) ->
+            acc + (if (current > last) 1 else 0)
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 1)
+    check(part2(testInput) == 0)
 
     val input = readInput("Day01")
     println(part1(input))
